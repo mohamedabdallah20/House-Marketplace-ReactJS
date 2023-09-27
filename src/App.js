ba2d32966c5d5fc0d246cf81a9c3db4e3141da1f
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-09-27 17:32:21 +0300

    initialize the basic pages so far

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Offers from './pages/Offers'
+import ForgotPassword from './pages/ForgotPassword'
+
 function App() {
-  return <div className="App">REACT_APP</div>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* Nav Bar */}
+    </>
+  )
 }
 
 export default App
