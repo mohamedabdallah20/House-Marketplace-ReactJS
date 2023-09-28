625082627e4cf45f9b13a1121ea7c2b008b80950
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-09-28 16:58:01 +0300

    add Edit Profile functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
+import { doc, updateDoc } from 'firebase/firestore'
+import { toast } from 'react-toastify'
+
 function Profile() {
   const auth = getAuth()
   const navigate = useNavigate()
 
+  const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -14,6 +19,32 @@ function Profile() {
   const onLogout = () => {
     auth.signOut()
     navigate('/')
+  }
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Update display name in firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+
+        // Update in firestore
+        const docRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(docRef, {
+          name,
+        })
+      }
+    } catch (error) {
+      toast.error('something went wrong')
+    }
+  }
+  const onChange = (e) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.id]: e.target.value,
+      }
+    })
   }
 
   return (
@@ -24,6 +55,40 @@ function Profile() {
           Logout
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="personalDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prev) => !prev)
+            }}
+          >
+            {changeDetails ? 'done' : 'edit'}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form action="">
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              id="email"
+              className="profileEmail"
+              disabled={true}
+              value={email}
+              // onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
