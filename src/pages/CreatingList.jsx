5ee26c899f6b6cd9392045a3f47c2025b5be4459
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-09-30 14:06:51 +0300

    Add upload images functionality

--- src/pages/CreatingList.jsx
+++ src/pages/CreatingList.jsx
@@ -103,6 +103,61 @@ function CreatingList() {
       geolocation.lat = latitude
       geolocation.lng = longitude
     }
+
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage()
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        const storageRef = ref(storage, `images/${fileName}`)
+        const uploadTask = uploadBytesResumable(storageRef, image)
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            console.log('Upload is ' + progress + '% done')
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused')
+                break
+              case 'running':
+                console.log('Upload is running')
+                break
+              default:
+                break
+            }
+          },
+          (error) => {
+            reject(error)
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL)
+            })
+          }
+        )
+      })
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false)
+      toast.error('Images not uploaded')
+      return
+    })
+    console.log(imgUrls)
+    // const formDataCopy = {
+    //   ...formData,
+    //   imageUrls:imgUrls,
+    //   geolocation,
+    //   timestamp: serverTimestamp(),
+    // }
+    setLoading(false)
+    // console.log(geolocation)
   }
   const onMutate = (e) => {
     let boolean = null
