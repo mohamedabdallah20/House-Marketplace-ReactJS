7a2cf11ca8c3a12d74cf3892c9e8b66dd7af2583
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2023-10-03 16:07:21 +0300

    Add profile own listings list and delete functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,50 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { db } from '../firebase.config'
-import { doc, updateDoc } from 'firebase/firestore'
+import {
+  doc,
+  updateDoc,
+  collection,
+  query,
+  getDocs,
+  where,
+  orderBy,
+  deleteDoc,
+} from 'firebase/firestore'
 import { toast } from 'react-toastify'
 import aroowKeyIcon from '../assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
+import ListingItem from '../components/ListingItem'
 
 function Profile() {
   const auth = getAuth()
   const navigate = useNavigate()
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    const fetchLstings = async () => {
+      const colRef = collection(db, 'listings')
+      const q = query(
+        colRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      )
+      const querySnap = await getDocs(q)
+      const listing = []
+      querySnap.forEach((doc) => {
+        return listing.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+      setListings(listing)
+      setLoading(false)
+    }
+    fetchLstings()
+  }, [auth.currentUser.uid])
   const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -48,7 +81,15 @@ function Profile() {
       }
     })
   }
-
+  const onDelete = async (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', id))
+      setListings((perv) => {
+        return perv.filter((listing) => listing.id !== id)
+      })
+      toast.success('Listing deleted successfully')
+    }
+  }
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -95,6 +136,21 @@ function Profile() {
           <p>sell or rent home</p>
           <img src={aroowKeyIcon} alt="arrow" />
         </Link>
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className="listingText">Your lIstings</p>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  id={listing.id}
+                  key={listing.id}
+                  listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   )
